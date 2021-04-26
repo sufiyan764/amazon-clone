3c7028b301a7203ce58704b2ad9aab0374cfa1d7
Sufiyan Shah <sufiyan764@gmail.com>
2021-04-26 21:34:03 +0530

    implemented state and dispatch functionality in product.js to dispatch the item into the data layer

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,24 @@
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
-const Product = ({title, image, price, rating}) => {
+const Product = ({ id, title, image, price, rating }) => {
+
+    const [state, dispatch] = useStateValue;
+
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price, 
+                rating: rating
+            }
+        })
+    }
+
     return (  
         <div className="product">
             <div className="product_info">
@@ -20,7 +38,7 @@ const Product = ({title, image, price, rating}) => {
                 src={image}
                 alt = 'product'
             />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     );
 }
